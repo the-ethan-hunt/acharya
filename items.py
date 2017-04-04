cd366975fd7803638eac6b8fefe24bc727c918b9
Dhruv Apte <dhruvgirishapte@gmail.com>
2017-04-04 21:36:29 +0530

    Update items.py

--- items.py
+++ items.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 import scrapy
 from scrapy.item import Item,Field
-class ReverseItem(scrapy.item):
-    defining the fields for items:
 class ReverseItem(scrapy.Item):
     #defining the fields for items
     name=Field()
